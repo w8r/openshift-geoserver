fa7889103566174b29d688254ebd75eddcf2ad4f
w8r <info@w8r.name>
2014-09-17 18:07:22 +0200

    better dialogs

--- install.sh
+++ install.sh
@@ -12,25 +12,24 @@ unzip -q ./geoserver-tmp/geoserver.zip -d ./webapps/geoserver
 rm -rf geoserver-tmp
 rm geoserver.zip
 
-echo "Enable JSONP? [y/n] (n): ";
-read answer
+read -p "Enable JSONP? [y/n] (n): " answer;
 
 # enbale JSONP
 if [ "$answer" == "y" ]
 then
-    echo "Enabling JSONP output...";
     # sed is insane
     sed -e ':a' -e 'N' -e '$!ba' -e 's/<!--\n*[ ]*\(<[^>]*>\n*[ ]*<[^>]*>ENABLE_JSONP<\/[^>]*>[ ]*\n*[ ]*<[^>]*>[^<]*<\/[^>]*>\n*[ ]*<[^>]*>[ ]*\n*[ ]*\)-->/\1/g' ./webapps/geoserver/WEB-INF/web.xml > ./webapps/geoserver/WEB-INF/w.xml
     cat ./webapps/geoserver/WEB-INF/w.xml > ./webapps/geoserver/WEB-INF/web.xml
     rm ./webapps/geoserver/WEB-INF/w.xml
+    echo "  * JSONP output enabled";
 fi
 
-echo "Remove pom.xml to avoid Maven builds? [y/n] (n): ";
-read answer
+read -p "Remove pom.xml to avoid Maven builds? [y/n] (n): " answer
 
 if [ "$answer" == "y" ]
 then
-rm ./pom.xml
+    rm ./pom.xml
+    echo "  * pom.xml deleted"
 fi
 
 echo "Done, geoserver is in the webapps folder, you can commit & deploy"
