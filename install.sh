8bd550301508af628e701a7cc5ce184aab560510
w8r <info@w8r.name>
2015-06-03 02:28:35 +0200

    stable/maintain verions, plugins added

--- install.sh
+++ install.sh
@@ -1,35 +1,66 @@
 #!/bin/sh
 
-# download the file from distro
-echo "Downloading Geoserver 2.6.3 from sourceforge.net..."
-curl -L http://sourceforge.net/projects/geoserver/files/GeoServer/2.6.3/geoserver-2.6.3-war.zip > geoserver.zip
-
-echo "Extracting the .war ...";
-mkdir -p geoserver-tmp
-unzip -q geoserver.zip -d geoserver-tmp
-mv ./geoserver-tmp/geoserver.war ./geoserver-tmp/geoserver.zip
-unzip -q ./geoserver-tmp/geoserver.zip -d ./webapps/geoserver
-rm -rf geoserver-tmp
-rm geoserver.zip
-
-read -p "Enable JSONP? [y/n] (n): " answer;
-
-# enbale JSONP
-if [ "$answer" == "y" ]
+read -r -p "Release: Stable/Maintenance? [S/m]: " response
+if [[ $response =~ ^([mM])$ ]]
 then
-    # sed is insane
-    sed -e ':a' -e 'N' -e '$!ba' -e 's/<!--\n*[ ]*\(<[^>]*>\n*[ ]*<[^>]*>ENABLE_JSONP<\/[^>]*>[ ]*\n*[ ]*<[^>]*>[^<]*<\/[^>]*>\n*[ ]*<[^>]*>[ ]*\n*[ ]*\)-->/\1/g' ./webapps/geoserver/WEB-INF/web.xml > ./webapps/geoserver/WEB-INF/w.xml
-    cat ./webapps/geoserver/WEB-INF/w.xml > ./webapps/geoserver/WEB-INF/web.xml
-    rm ./webapps/geoserver/WEB-INF/w.xml
-    echo "  * JSONP output enabled";
+    release="maintain"
+else
+    release="stable"
 fi
 
-read -p "Remove pom.xml to avoid Maven builds? [y/n] (n): " answer
+echo "Getting link to $release.war..."
+html=$(curl -s "http://geoserver.org/release/${release}/")
+until [ -n "$html" ]
+do
+	sleep 10
+	html=$(curl -s "http://geoserver.org/release/${release}/")
+done
 
-if [ "$answer" == "y" ]
+base=$(echo "$html" | grep -o -E 'url = "([^"#]+)"' | cut -d'"' -f2)
+file=$(echo "$html" | grep -o -E 'artifact = "([^"]+)"' | cut -d'"' -f2)
+url="${base}${file}-war.zip"
+extensions_url="${base}extensions/"
+
+mkdir -p tmp
+echo "Downloading $url"
+curl -L "$url" > ./tmp/geoserver.zip
+unzip -q ./tmp/geoserver.zip -d ./tmp
+unzip -q ./tmp/geoserver.war -d ./webapps/geoserver
+
+read -r -p "Extensions? [y/N]: " response
+if [[ $response =~ ^([yY][eE][sS]|[yY])$ ]]
 then
-    rm ./pom.xml
-    echo "  * pom.xml deleted"
+	plugin_urls=$(curl -s "$extensions_url" | grep -o -E '<a href="([^"#]+)download"' | cut -d'"' -f2)
+	for url in $plugin_urls; do
+		plugin_name=${url/${extensions_url}${file}-/}
+		plugin_name=${plugin_name/\/download/}
+	
+		read -r -p " - ${plugin_name/\.zip/} [y/N]: " response
+		if [[ $response =~ ^([yY][eE][sS]|[yY])$ ]]
+		then
+			echo "Downloading ${plugin_name}"
+			curl -L "${url}" > tmp/$plugin_name
+			unzip -q tmp/$plugin_name -d ./webapps/geoserver/WEB-INF/lib/
+		fi
+	done
 fi
 
+read -p "Enable JSONP? [Y/n]: " response
+if ! [[ $response =~ ^([nN][oO]|[nN])$ ]]
+then
+	# sed is insane
+	sed -e ':a' -e 'N' -e '$!ba' -e 's/<!--\n*[ ]*\(<[^>]*>\n*[ ]*<[^>]*>ENABLE_JSONP<\/[^>]*>[ ]*\n*[ ]*<[^>]*>[^<]*<\/[^>]*>\n*[ ]*<[^>]*>[ ]*\n*[ ]*\)-->/\1/g' ./webapps/geoserver/WEB-INF/web.xml > ./webapps/geoserver/WEB-INF/w.xml
+	cat ./webapps/geoserver/WEB-INF/w.xml > ./webapps/geoserver/WEB-INF/web.xml
+	rm ./webapps/geoserver/WEB-INF/w.xml
+	echo "  * JSONP output enabled"
+fi
+
+read -p "Remove pom.xml to avoid Maven builds? [Y/n]: " response
+if ! [[ $response =~ ^([nN][oO]|[nN])$ ]]
+then
+	rm ./pom.xml
+	echo "  * pom.xml deleted"
+fi
+
+rm -rf ./tmp
 echo "Done, geoserver is in the webapps folder, you can commit & deploy"
